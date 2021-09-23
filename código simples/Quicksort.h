3deeb1a4574dbe60615079b2120bf7d261252546
RodrigoUchoa001 <85903922+RodrigoUchoa001@users.noreply.github.com>
2021-09-23 19:38:14 -0300

    Update Quicksort.h

--- código simples/Quicksort.h
+++ código simples/Quicksort.h
@@ -1,104 +1,18 @@
-#include "Quicksort.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include<time.h>
+#define MAX 100000
 
-//MÉTODOS DO QUICKSORT
-int escolheQuantValores(){
-    int quant;
-    scanf("%d",&quant);
-    
-    if (quant==0){
-        return 100;
-    } 
-    else if (quant==1){
-        return 1000;
-    }
-    else if (quant==2){ 
-        return 10000;
-    }
-    else if(quant==3){ 
-        return 100000;
-    }
-    else{
-        return -1;
-    }
-}
+int escolheQuantValores();
+int escolheFormaOrdenecao();
+void criaVetor(int *vetor, int quant,int formaOrd);
 
-int escolheFormaOrdenecao(){
-    int ord;
-    scanf("%d",&ord);
-    
-    if (ord==0){
-        return 0;
-    } 
-    else if (ord==1){
-        return 1;
-    }
-    else if (ord==2){ 
-        return 2;
-    }
-    else return -1;
-    
-}
+int geraNumero(int quant);
 
-void criaVetor(int *vetor, int quant,int formaOrd){
-    int valor; //VAI SER USADO NO CASO DE ALEATORIO
-    int posicao=0; //VAI SER USADO NO CASO DE DECRESCENTE
-    
-    switch (formaOrd){
-        case 0: //CRESCENTE
-            for (int i=0;i<quant;i++){
-                vetor[i]=i;
-            }
-            break;
-        case 1: //DECRESCENTE
-             //
-            for (int i=quant;i>0;i--){
-                vetor[posicao]=i;
-                posicao++;
-            }
-            break;
-        case 2: //ALEATORIO
-            
-            srand(time(NULL)); //NECESSARIO PARA SEMPRE QUANDO EXECUTAR VIR Ns DIFERENTES
-            
-            for (int i=0;i<quant;i++){
-                //valor=rand()%(quant*2); 
-                valor=geraNumero(quant);
-                vetor[i]=valor;
-            }
-    }
-    
-}
+void exibeVetor(int *vetor,int tamanho);
 
-int geraNumero(int quant){
-    int valor=rand()%quant; //SERÁ INSERIDO NO VETOR UM NUMERO ENTRE 0 E O DOBRO DO N DE POSICOES
-    return valor;
-}
+//ORDENACAO
+void ordenaQuickSort(int *vetor, int *nCompMovi, int primeiraPosi, int ultimaPosi);
+void trocaPosicoes(int *vetor, int posi1, int posi2);
+int particiona(int *vetor, int *nCompMovi, int inicio, int fim);
 
 
-void exibeVetor(int *vetor, int tamanho){
-    for (int i=0;i<tamanho;i++){
-        printf("posicao %d: %d\n",i,vetor[i]);
-    }
-}
-
-void ordenaQuickSort(int *vetor,int *nCompMovi){
-    //nCompMovi[0]=2; //NUMERO DE COMPARACOES
-    //nCompMovi[1]=3; //NUMERO DE MOVIMENTACOES
-    //vetor[0]=123;
-    
-    
-    
-    
-    
-    //return *vetor; NAO PRECISA
-}
-
-void copiaVetor(int *vetor, int *vetorAnterior, int quant){
-    for (int i=0;i<quant;i++){
-        vetorAnterior[i]=vetor[i];
-    }
-}
-
+//
+void copiaVetor(int *vetor, int *vetorAnterior, int quant);
