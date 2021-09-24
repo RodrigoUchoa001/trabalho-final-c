76d4732340320edd3a5a86cc94ea3d8212e6c5a8
RodrigoUchoa001 <85903922+RodrigoUchoa001@users.noreply.github.com>
2021-09-24 11:26:22 -0300

    Update Quicksort.h

--- código simples/Quicksort.h
+++ código simples/Quicksort.h
@@ -1,18 +1,19 @@
 #define MAX 100000
 
+//METODOS DE CRIACAO DO VETOR
 int escolheQuantValores();
 int escolheFormaOrdenecao();
 void criaVetor(int *vetor, int quant,int formaOrd);
-
 int geraNumero(int quant);
 
+//METODOS DE EXIBICAO DO VETOR
 void exibeVetor(int *vetor,int tamanho);
 
-//ORDENACAO
+//METODOS DE ORDENACAO DO VETOR
 void ordenaQuickSort(int *vetor, int *nCompMovi, int primeiraPosi, int ultimaPosi);
 void trocaPosicoes(int *vetor, int posi1, int posi2);
-int particiona(int *vetor, int *nCompMovi, int inicio, int fim);
+int particiona(int *vetor, int *nCompMovi, int primeiraPosi, int ultimaPosi);
 
 
-//
+//METODO DE COPIAR O VETOR
 void copiaVetor(int *vetor, int *vetorAnterior, int quant);
